7427e21cdb248f12b9eee9e188bcb923526fbfb7
pgonzalesc <pedro281195@gmail.com>
2020-11-07 11:59:34 -0500

    Ejercicio 3

--- src/components/Ejercicio2/index.js
+++ src/components/Ejercicio2/index.js
@@ -3,13 +3,16 @@ import './index.css';
 
 export const Ejercicio2 = ()=> {
     return (
-        <section className="container">
-            <div className="tittle-container">
+        <section className="container-news">
+            <div className="tittle-container-news">
                 <h1>Últimas</h1>
                 <h1>Noticias</h1>
                 <p>Noviembre 2020</p>
             </div>
             <div className="card">
+                <div className="card-image">
+                    <img src="https://arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/YFR3YQIG4FDXNCAOKLRGBLKANM.jpg" />
+                </div>
                 <div className="card-body">
                     <div className="news-date">
                         <p>05 Noviembre 2020</p>
@@ -19,6 +22,9 @@ export const Ejercicio2 = ()=> {
                     </div>
                     <div className="news-body">
                         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    </div>
+                    <div className="news-footer">
+                        <p>Find out more</p>
                     </div>
                 </div>
             </div>
--- src/components/Ejercicio3/index.js
+++ src/components/Ejercicio3/index.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './index.css';
 
 export const Ejercicio3 = ()=> {
+    const [data, setData] = useState('');
+    const [error, setError] = useState('');
+
+    const { name, cell, email, comment } = data;
+
+    const temp = {}
+    const msgErr = {}
+
+    const reset = () => {
+        document.querySelector('#name').value = '';
+        document.querySelector('#cell').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#comment').value = '';
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        let ind = '1';
+
+        if(!temp.name){
+            ind = '0';
+            msgErr.name = 'El campo nombre es obligatorio';
+        }else if(!temp.cell){
+            ind = '0';
+            msgErr.cell = 'El campo teléfono es obligatorio';
+        }else if(!temp.email){
+            ind = '0';
+            msgErr.email = 'El campo correo es obligatorio';
+        }else if(!temp.comment){
+            ind = '0';
+            msgErr.comment = 'El campo comentario es obligatorio';
+        }
+        
+        if(ind === '0')
+            setError(msgErr);
+        else{
+            //document.querySelector('#dataBody').style.display = 'flex';
+            setData(temp);
+            reset();
+        }
+
+        
+    }
+
+    const handleChange = (e, name) => {
+        temp[name] = e.target.value;
+    }
+
     return (
-        <div>Ejercicio 3</div>
+        <section className="container-form">
+            <div className="form">
+                <div className="form-data">
+                    <input type="text" id="name" name="name" placeholder="Nombre" required onChange={(e)=>{handleChange(e, 'name')}} autoComplete="off"/>
+                    <p className="errorMsg">{error.name}</p>
+                    <input type="text" id="cell" name="cell"  placeholder="Número de Teléfono" required onChange={(e)=>{handleChange(e, 'cell')}} autoComplete="off"/>
+                    <p className="errorMsg">{error.cell}</p>
+                    <input type="text" id="email" name="email" placeholder="Correo" required onChange={(e)=>{handleChange(e, 'email')}} autoComplete="off"/>
+                    <p className="errorMsg">{error.email}</p>
+                    <input type="text" id="comment" name="comment" placeholder="Comentario" required onChange={(e)=>{handleChange(e, 'comment')}} autoComplete="off"/>
+                    <p className="errorMsg">{error.comment}</p>
+                    <p>{name}</p>
+                    <p>{cell}</p>
+                    <p>{email}</p>
+                    <p>{comment}</p>
+                </div>
+                <div className="form-btn">
+                    <button onClick={handleForm}>Send</button>
+                </div>
+            </div>
+        </section>
     )
 }
